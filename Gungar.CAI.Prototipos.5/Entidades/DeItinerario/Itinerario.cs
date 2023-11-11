ec0a5d34cba3843ec676101bbde41c62686b2713
Mariano <mendez.mariano@outlook.com.ar>
2023-11-11 16:57:38 -0300

    Borrado de metodo DiasEntreFechas duplicado sin referencias

--- Gungar.CAI.Prototipos.5/Entidades/DeItinerario/Itinerario.cs
+++ Gungar.CAI.Prototipos.5/Entidades/DeItinerario/Itinerario.cs
@@ -107,14 +107,6 @@ namespace Gungar.CAI.Prototipos._5.Entidades.DeItinerario
             VuelosAgregados.Remove(reserva);
         }
 
-        private int DiasEntreFechas(DateTime FechaDesde, DateTime FechaHasta)
-        {
-            TimeSpan diferencia = FechaHasta.Date - FechaDesde.Date;
-
-            int dias = diferencia.Days;
-
-            return dias;
-        }
         public float CalcularPrecioTotal()
         {
             float precioTotal = 0;
