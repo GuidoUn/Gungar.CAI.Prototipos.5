2f12c6706556cc08de4f31aa1243e34d6ec2475c
Daniela Martinez <danielamartinezsn@gmail.com>
2023-11-12 18:37:59 -0300

    Cambio de diseño boton Continuar por Guardar en ClienteForm

--- Gungar.CAI.Prototipos.5/Forms/DeItinerario/Cliente/ClienteForm.Designer.cs
+++ Gungar.CAI.Prototipos.5/Forms/DeItinerario/Cliente/ClienteForm.Designer.cs
@@ -57,21 +57,18 @@
             nuevoItinerarioBox.Controls.Add(apellidoText);
             nuevoItinerarioBox.Controls.Add(label3);
             nuevoItinerarioBox.Controls.Add(nuevoPasajeroText);
-            nuevoItinerarioBox.Location = new Point(58, 87);
-            nuevoItinerarioBox.Margin = new Padding(3, 4, 3, 4);
+            nuevoItinerarioBox.Location = new Point(51, 65);
             nuevoItinerarioBox.Name = "nuevoItinerarioBox";
-            nuevoItinerarioBox.Padding = new Padding(3, 4, 3, 4);
-            nuevoItinerarioBox.Size = new Size(334, 401);
+            nuevoItinerarioBox.Size = new Size(292, 301);
             nuevoItinerarioBox.TabIndex = 21;
             nuevoItinerarioBox.TabStop = false;
             nuevoItinerarioBox.Text = "Datos de cliente";
             // 
             // telefonoText
             // 
-            telefonoText.Location = new Point(14, 348);
-            telefonoText.Margin = new Padding(3, 4, 3, 4);
+            telefonoText.Location = new Point(12, 261);
             telefonoText.Name = "telefonoText";
-            telefonoText.Size = new Size(301, 27);
+            telefonoText.Size = new Size(264, 23);
             telefonoText.TabIndex = 21;
             telefonoText.TextChanged += telefonoText_TextChanged;
             telefonoText.KeyPress += telefonoText_KeyPress;
@@ -79,36 +76,34 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(7, 324);
+            label4.Location = new Point(6, 243);
             label4.Name = "label4";
-            label4.Size = new Size(67, 20);
+            label4.Size = new Size(52, 15);
             label4.TabIndex = 20;
             label4.Text = "Telefono";
             // 
             // emailText
             // 
-            emailText.Location = new Point(14, 276);
-            emailText.Margin = new Padding(3, 4, 3, 4);
+            emailText.Location = new Point(12, 207);
             emailText.Name = "emailText";
-            emailText.Size = new Size(301, 27);
+            emailText.Size = new Size(264, 23);
             emailText.TabIndex = 19;
             emailText.TextChanged += emailText_TextChanged;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(7, 252);
+            label2.Location = new Point(6, 189);
             label2.Name = "label2";
-            label2.Size = new Size(46, 20);
+            label2.Size = new Size(36, 15);
             label2.TabIndex = 18;
             label2.Text = "Email";
             // 
             // documentoText
             // 
-            documentoText.Location = new Point(14, 205);
-            documentoText.Margin = new Padding(3, 4, 3, 4);
+            documentoText.Location = new Point(12, 154);
             documentoText.Name = "documentoText";
-            documentoText.Size = new Size(301, 27);
+            documentoText.Size = new Size(264, 23);
             documentoText.TabIndex = 17;
             documentoText.TextChanged += documentoText_TextChanged;
             documentoText.KeyPress += documentoText_KeyPress;
@@ -116,27 +111,26 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(7, 181);
+            label5.Location = new Point(6, 136);
             label5.Name = "label5";
-            label5.Size = new Size(248, 20);
+            label5.Size = new Size(198, 15);
             label5.TabIndex = 14;
             label5.Text = "Número de Documento o Pasaporte";
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(7, 109);
+            label6.Location = new Point(6, 82);
             label6.Name = "label6";
-            label6.Size = new Size(66, 20);
+            label6.Size = new Size(51, 15);
             label6.TabIndex = 16;
             label6.Text = "Apellido";
             // 
             // apellidoText
             // 
-            apellidoText.Location = new Point(14, 133);
-            apellidoText.Margin = new Padding(3, 4, 3, 4);
+            apellidoText.Location = new Point(12, 100);
             apellidoText.Name = "apellidoText";
-            apellidoText.Size = new Size(301, 27);
+            apellidoText.Size = new Size(264, 23);
             apellidoText.TabIndex = 15;
             apellidoText.TextChanged += apellidoText_TextChanged;
             apellidoText.KeyPress += apellidoText_KeyPress;
@@ -144,18 +138,17 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(7, 39);
+            label3.Location = new Point(6, 29);
             label3.Name = "label3";
-            label3.Size = new Size(64, 20);
+            label3.Size = new Size(51, 15);
             label3.TabIndex = 10;
             label3.Text = "Nombre";
             // 
             // nuevoPasajeroText
             // 
-            nuevoPasajeroText.Location = new Point(14, 63);
-            nuevoPasajeroText.Margin = new Padding(3, 4, 3, 4);
+            nuevoPasajeroText.Location = new Point(12, 47);
             nuevoPasajeroText.Name = "nuevoPasajeroText";
-            nuevoPasajeroText.Size = new Size(301, 27);
+            nuevoPasajeroText.Size = new Size(264, 23);
             nuevoPasajeroText.TabIndex = 9;
             nuevoPasajeroText.TextChanged += nuevoPasajeroText_TextChanged;
             nuevoPasajeroText.KeyPress += nuevoPasajeroText_KeyPress;
@@ -163,18 +156,17 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(58, 36);
+            label1.Location = new Point(51, 27);
             label1.Name = "label1";
-            label1.Size = new Size(117, 20);
+            label1.Size = new Size(91, 15);
             label1.TabIndex = 19;
             label1.Text = "Datos de cliente";
             // 
             // button1
             // 
-            button1.Location = new Point(232, 507);
-            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Location = new Point(51, 380);
             button1.Name = "button1";
-            button1.Size = new Size(160, 88);
+            button1.Size = new Size(140, 40);
             button1.TabIndex = 22;
             button1.Text = "Cancelar";
             button1.UseVisualStyleBackColor = true;
@@ -182,25 +174,26 @@
             // 
             // continuarBtn
             // 
-            continuarBtn.Location = new Point(58, 507);
-            continuarBtn.Margin = new Padding(3, 4, 3, 4);
+            continuarBtn.BackColor = SystemColors.HotTrack;
+            continuarBtn.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            continuarBtn.ForeColor = SystemColors.ButtonFace;
+            continuarBtn.Location = new Point(203, 380);
             continuarBtn.Name = "continuarBtn";
-            continuarBtn.Size = new Size(160, 88);
+            continuarBtn.Size = new Size(140, 40);
             continuarBtn.TabIndex = 20;
-            continuarBtn.Text = "Continuar";
-            continuarBtn.UseVisualStyleBackColor = true;
+            continuarBtn.Text = "Guardar";
+            continuarBtn.UseVisualStyleBackColor = false;
             continuarBtn.Click += continuarBtn_Click;
             // 
             // ClienteForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1093, 937);
+            ClientSize = new Size(441, 497);
             Controls.Add(nuevoItinerarioBox);
             Controls.Add(label1);
             Controls.Add(button1);
             Controls.Add(continuarBtn);
-            Margin = new Padding(3, 4, 3, 4);
             Name = "ClienteForm";
             Text = "ClienteForm";
             Load += ClienteForm_Load;
